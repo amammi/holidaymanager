9b61db19a8dbfc7aa33af273f36d5ce5b81560dd
Andrea Mammì <andrea.mammi@bancacfplus.it>
2022-08-01 15:22:17 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Holiday Manager",
+    author="Andrea Mammì",
+    license=open("LICENSE", mode='r').read(),
+    description="Python module for italian holidays",
+    version="1.0.0"
+)
